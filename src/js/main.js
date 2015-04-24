9a7f88885c02eb9bfbd72c82ddb131edf2022e0e
ogousa <ogousa@gmail.com>
2015-04-24 17:56:32 -0400

    albums update

--- src/js/main.js
+++ src/js/main.js
@@ -46,3 +46,21 @@ function runCarousel($scope){
     {image: 'images/Ringo.png', caption: "RINGO", id: "ringo"}
   ];
 }
+
+function runAlbums($scope){
+  $scope.albums = [
+    {image: "images/albums/PleasePleaseMe.jpg", title: "Please Please Me", date: "22 March 1963", id: "01"},
+    {image: "images/albums/WithTheBeatles.jpg", title: "With The Beatles", date: "22 November 1963", id: "02"},
+    {image: "images/albums/HardDay'sNight.jpg", title: "A Hard Day's Night", date: "26 June 1964", id: "03"},
+    {image: "images/albums/BeatlesForSale.jpg", title: "Beatles For Sale", date: "4 December 1964", id: "04"},
+    {image: "images/albums/Help.jpg", title: "Help!", date: "6 August 1965", id: "05"},
+    {image: "images/albums/RubberSoul.jpg", title: "Rubber Soul", date: "3 December 1965", id: "06"},
+    {image: "images/albums/Revolver.jpg", title: "Revolver", date: "5 August 1966", id: "07"},
+    {image: "images/albums/Sgt.Pepper'sLonelyHeartsClubBand.jpg", title: "Sgt. Pepper's Lonely Hearts Club Band", date: "1 June 1967", id: "08"},
+    {image: "images/albums/MagicalMysteryTour.jpg", title: "Magical Mystery Tour", date: "27 November 1967", id: "09"},
+    {image: "images/albums/TheBeatles.jpg", title: "The Beatles", date: "22 November 1968", id: "10"},
+    {image: "images/albums/YellowSubmarine.jpg", title: "Yellow Submarine", date: "17 January 1969", id: "11"},
+    {image: "images/albums/AbbeyRoad.jpg", title: "Abbey Road", date: "26 September 1969", id: "12"},
+    {image: "images/albums/LetItBe.jpg", title: "Let It Be", date: "8 May 1970", id: "13"},
+  ];
+}
