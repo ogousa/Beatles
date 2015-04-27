f1527ec8617cc6504691b09280aeba8b60782aef
ogousa <ogousa@gmail.com>
2015-04-27 10:07:29 -0400

    pass album ID to album page

--- src/js/main.js
+++ src/js/main.js
@@ -50,19 +50,19 @@ function runCarousel($scope){
 
 app.factory('albumsInfo', function() {  
     return {albums: [
-    {image: "images/albums/PleasePleaseMe.jpg", title: "Please Please Me", date: "22 March 1963", id: "#/album/0"},
-    {image: "images/albums/WithTheBeatles.jpg", title: "With The Beatles", date: "22 November 1963", id: "#/album/1"},
-    {image: "images/albums/HardDay'sNight.jpg", title: "A Hard Day's Night", date: "26 June 1964", id: "#/album/2"},
-    {image: "images/albums/BeatlesForSale.jpg", title: "Beatles For Sale", date: "4 December 1964", id: "#/album/3"},
-    {image: "images/albums/Help.jpg", title: "Help!", date: "6 August 1965", id: "#/album/4"},
-    {image: "images/albums/RubberSoul.jpg", title: "Rubber Soul", date: "3 December 1965", id: "#/album/5"},
-    {image: "images/albums/Revolver.jpg", title: "Revolver", date: "5 August 1966", id: "#/album/6"},
-    {image: "images/albums/Sgt.Pepper'sLonelyHeartsClubBand.jpg", title: "Sgt. Pepper's Lonely Hearts Club Band", date: "1 June 1967", id: "#/album/7"},
-    {image: "images/albums/MagicalMysteryTour.jpg", title: "Magical Mystery Tour", date: "27 November 1967", id: "#/album/8"},
-    {image: "images/albums/TheBeatles.jpg", title: "The Beatles", date: "22 November 1968", id: "#/album/9"},
-    {image: "images/albums/YellowSubmarine.jpg", title: "Yellow Submarine", date: "17 January 1969", id: "#/album/10"},
-    {image: "images/albums/AbbeyRoad.jpg", title: "Abbey Road", date: "26 September 1969", id: "#/album/11"},
-    {image: "images/albums/LetItBe.jpg", title: "Let It Be", date: "8 May 1970", id: "#/album/12"},
+    {image: "images/albums/PleasePleaseMe.jpg", title: "Please Please Me", date: "22 March 1963", id: "#/album/1"},
+    {image: "images/albums/WithTheBeatles.jpg", title: "With The Beatles", date: "22 November 1963", id: "#/album/2"},
+    {image: "images/albums/HardDay'sNight.jpg", title: "A Hard Day's Night", date: "26 June 1964", id: "#/album/3"},
+    {image: "images/albums/BeatlesForSale.jpg", title: "Beatles For Sale", date: "4 December 1964", id: "#/album/4"},
+    {image: "images/albums/Help.jpg", title: "Help!", date: "6 August 1965", id: "#/album/5"},
+    {image: "images/albums/RubberSoul.jpg", title: "Rubber Soul", date: "3 December 1965", id: "#/album/6"},
+    {image: "images/albums/Revolver.jpg", title: "Revolver", date: "5 August 1966", id: "#/album/7"},
+    {image: "images/albums/Sgt.Pepper'sLonelyHeartsClubBand.jpg", title: "Sgt. Pepper's Lonely Hearts Club Band", date: "1 June 1967", id: "#/album/8"},
+    {image: "images/albums/MagicalMysteryTour.jpg", title: "Magical Mystery Tour", date: "27 November 1967", id: "#/album/9"},
+    {image: "images/albums/TheBeatles.jpg", title: "The Beatles", date: "22 November 1968", id: "#/album/10"},
+    {image: "images/albums/YellowSubmarine.jpg", title: "Yellow Submarine", date: "17 January 1969", id: "#/album/11"},
+    {image: "images/albums/AbbeyRoad.jpg", title: "Abbey Road", date: "26 September 1969", id: "#/album/12"},
+    {image: "images/albums/LetItBe.jpg", title: "Let It Be", date: "8 May 1970", id: "#/album/13"},
   ]};
 });
 
@@ -73,9 +73,11 @@ function runAlbums($scope, albumsInfo){
 //app.controller('albumController', function($scope, $routeParams) {
 function albumController($scope, $routeParams, albumsInfo) {
  
-    $scope.albumId = $routeParams.id;
+    $scope.id = 1;
+    $scope.albumId = parseInt($routeParams.id);
     $scope.image = albumsInfo.albums[$scope.albumId].image;
     $scope.title = albumsInfo.albums[$scope.albumId].title;
     $scope.date = albumsInfo.albums[$scope.albumId].date;
-
+//    $scope.songs = "partials/a_" + $scope.albumId + ".html";
+    $scope.songs = '"partials/a_1.html"';
 };
