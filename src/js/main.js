448fd75f1ad3d0c8a117e6a1b82c9533accd12b2
ogousa <ogousa@gmail.com>
2015-05-21 15:19:30 -0400

    moving controllers to separate files

--- src/js/main.js
+++ src/js/main.js
@@ -9,6 +9,7 @@ var app = angular.module('BeatlesApp', [
     'ngAnimate', 
     'findText', 
     'carouselController', 
+    'pageController', 
     'albumController', 
     'albumsController', 
     'songController',
@@ -25,40 +26,13 @@ app.config(['$routeProvider',
         $routeProvider
         .when("/",          {templateUrl: "partials/home.html",     controller: "PageController"})
         .when("/home",      {templateUrl: "partials/home.html",     controller: "PageController"})
+        .when("/about",     {templateUrl: "partials/about.html",    controller: "PageController"})
         .when("/albums",    {templateUrl: "partials/albums.html",   controller: "AlbumsController"})
         .when("/songs",     {templateUrl: "partials/songs.html",    controller: "SongController"})
-        .when("/about",     {templateUrl: "partials/about.html",    controller: "PageController"})
-        .when("/album/:id", {templateUrl: "partials/album.html",    controller: "AlbumController"})
         .when("/song",      {templateUrl: "partials/song.html",     controller: "SongController"})
+        .when("/album/:id", {templateUrl: "partials/album.html",    controller: "AlbumController"})
         // else 404
         .otherwise("/",     {templateUrl: "partials/home.html",     controller: "PageController"});
-    }
-]);
-
-/**
- * Controls all other Pages
- */
-app.controller('PageController', ['$scope', '$location', '$anchorScroll', 'anchorSmoothScroll',
-    function ($scope, $location, $anchorScroll, anchorSmoothScroll) {
-        $scope.gotoAnchor = function(x) {
-            var newHash = x;
-            if ($location.hash() !== newHash) {
-                // set the $location.hash to newHash and $anchorScroll will automatically scroll to it
-                $location.hash(x);
-            } else {
-                // call $anchorScroll() explicitly, since $location.hash hasn't changed
-                $anchorScroll();
-            }
-        };
-
-        $scope.gotoElement = function (eID){
-            // set the location.hash to the id of
-            // the element you wish to scroll to.
-            $location.hash(eID);
-     
-            // call $anchorScroll()
-            anchorSmoothScroll.scrollTo(eID);
-        };
     }
 ]);
 
